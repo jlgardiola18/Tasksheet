671b61988a1b568a8e89ffac8e7466d9a7e80f7a
JL Gardiola <88923726+jlgardiola18@users.noreply.github.com>
2024-10-08 07:48:07 +0000

    task113 done

---
+++ Tasksheet113/Main.java
@@ -0,0 +1,19 @@
+package Tasksheet113;
+
+public class Main {
+
+    public static void main(String[] args) {
+      byte three = 3;
+      byte one = 1;
+      byte zero = 0;
+      float version = 2.0f;
+      boolean condition = true; 
+    
+      String output = "H" + three + one + one + zero + " " + "w" + zero + "r" + one + "d" + " " + version + " " + condition;
+      System.out.println(output);
+
+    //    byte zero = 0;
+    //      String output = "W" + zero + "w";
+    //      System.out.println(output);
+    }
+  }
