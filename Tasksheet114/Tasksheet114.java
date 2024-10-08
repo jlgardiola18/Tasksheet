7857182d5e82fb889d1443abc8d3a4f99beaea7c
JL Gardiola <88923726+jlgardiola18@users.noreply.github.com>
2024-10-08 09:53:20 +0000

    task114 done

---
+++ Tasksheet114/Tasksheet114.java
@@ -0,0 +1,27 @@
+package Tasksheet114;
+import java.util.*;
+
+public class Tasksheet114 {
+    @SuppressWarnings("resource")
+    public static void main(String[] args) {
+
+        int check_number = 10;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("\nEnter a number: ");
+        check_number = sc.nextInt();
+
+        for (int i = 1; i <= check_number; i++) {
+            if ((i % 2) == 0) {
+                String message = i + " is even number";
+                System.out.println(message);
+                // System.out.print(i + " is even number");
+            } else if ((check_number % 2) >= 0) {
+                String message = i + " is odd number";
+                System.out.println(message);
+                // System.out.print("\n" + i + " is odd number");
+            }
+        }
+    }
+}
